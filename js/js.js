48c8630b6ceb4d14ad69823ef08b9469b895f036
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-03-21 09:28:41 +0100

    fået kort ind

--- js/js.js
+++ js/js.js
@@ -2,3 +2,14 @@
 fil: js.js 
 formål: at bruge leaflet (https://leafletjs.com/) til at sætte markør på kort integeret med openstreetmap
 */
+
+//,13 = hvor meget zoomet ind vi er på kortet
+var map = L.map('map').setView([56.15, 10.22], 13);
+
+//copyright filer
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+//&copy; = copy symbol
+
